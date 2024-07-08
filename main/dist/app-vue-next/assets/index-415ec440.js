4f42ebe7eedda5377def83cd13267442b9180ca5
xlan <xlan@sonicwall.com>
2024-07-08 17:38:49 +0800

    fix: upload build sub app source file

---
+++ main/dist/app-vue-next/assets/index-415ec440.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-cd1f3e10.js";const t={};function o(r,s){return c(),n("h4",null,"This is app-vue-next app")}const _=e(t,[["render",o]]);export{_ as default};
---
+++ main/dist/assets/index-674d6dcb.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-c3c8ad0d.js";const o={};function r(s,t){return c(),n("h4",null,"This is main home page")}const _=e(o,[["render",r]]);export{_ as default};
